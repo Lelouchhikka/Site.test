3271b9da7130b8c42e1f53a1b00ece16af3d89c9
Lelouchhikka <kelmanov-e@istep.kz>
2021-04-23 19:42:05 +0600

    Opinion

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,54 +1,28 @@
 @extends('layouts.app')
 @section('contentRight')
     <div class="container" style="margin-top: 80px">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                @if(session()->get('locale')=='ru')
-                    {{(App::setLocale('ru'))}}
-                @endif
-                <li class="breadcrumb-item active" aria-current="page">{{__('Home ')}}</li>
-                <li class="breadcrumb-item active"> <a href="{{route("category.list")}}">{{__('Categories')}}</a></li>
-                <li class="breadcrumb-item active"> <a href="{{route("brand.list")}}">{{__('Brands')}}</a></li>
-            </ol>
-        </nav>
         <div class="row justify-content-center">
-            <div class="col-lg-12">
-                <div class="row">
-                    <div class="col-lg-7">
-                        <h4>{{__("Products In Our Store")}}</h4>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    @foreach($products as $pro)
-                        <div class="col-lg-3">
-                            <div class="card" style="margin-bottom: 20px; height: auto;">
-                                <img src="/images/{{ $pro->image_path }}"
-                                     class="card-img-top mx-auto"
-                                     style="height: 150px; width: 150px;display: block;"
-                                     alt="{{ $pro->image_path }}"
-                                >
+            <div class="col-lg-10 " >
+
+                <div class="row border">
+                    @foreach($opinions as $opinion)
+
+                        <div class="col w-75 h-75 border-bottom " align="center">
+
+                                <h6 class="card-title">{{ $opinion->articleName }}</h6>
                                 <div class="card-body">
-                                    <a href=""><h6 class="card-title">{{ $pro->name }}</h6></a>
-                                    <p>${{ $pro->price }}</p>
-                                    <form action="{{ route('cart.store') }}" method="POST">
-                                        {{ csrf_field() }}
-                                        <input type="hidden" value="{{ $pro->id }}" id="id" name="id">
-                                        <input type="hidden" value="{{ $pro->name }}" id="name" name="name">
-                                        <input type="hidden" value="{{ $pro->price }}" id="price" name="price">
-                                        <input type="hidden" value="{{ $pro->image_path }}" id="img" name="img">
-                                        <input type="hidden" value="{{ $pro->slug }}" id="slug" name="slug">
-                                        <input type="hidden" value="1" id="quantity" name="quantity">
-                                        <div class="card-footer" style="background-color: white;">
-                                            <div class="row">
-                                                <button class="btn btn-secondary btn-sm" class="tooltip-test" title="add to cart">
-                                                    <i class="fa fa-shopping-cart"></i> {{__("add to cart")}}
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
+                                <img src="/images/{{DB::table('users')->where('id', $opinion->userId)->first()->image_path}}"
+                                     class="card-img w-25 h-25 rounded-circle"
+                                     style="height: 150px; width: 150px;display: block;"
+                                     alt="{{ DB::table('users')->where('id', $opinion->userId)->first()->image_path }}"
+                                >
+
+
+                                    <p>{{ $opinion->description }}</p>
+                                    <p>by {{ DB::table('users')->where('id', $opinion->userId)->first()->name }}</p>
+                                     <p>   {{$opinion->date}}</p>
+
                                 </div>
-                            </div>
                         </div>
                     @endforeach
                 </div>
@@ -57,116 +31,6 @@
     </div>
 @endsection
 @section('contentMid')
-    <div class="container" style="margin-top: 80px">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                @if(session()->get('locale')=='ru')
-                    {{(App::setLocale('ru'))}}
-                @endif
-                <li class="breadcrumb-item active" aria-current="page">{{__('Home ')}}</li>
-                <li class="breadcrumb-item active"> <a href="{{route("category.list")}}">{{__('Categories')}}</a></li>
-                <li class="breadcrumb-item active"> <a href="{{route("brand.list")}}">{{__('Brands')}}</a></li>
-            </ol>
-        </nav>
-        <div class="row justify-content-center">
-            <div class="col-lg-12">
-                <div class="row">
-                    <div class="col-lg-7">
-                        <h4>{{__("Products In Our Store")}}</h4>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    @foreach($products as $pro)
-                        <div class="col-lg-3">
-                            <div class="card" style="margin-bottom: 20px; height: auto;">
-                                <img src="/images/{{ $pro->image_path }}"
-                                     class="card-img-top mx-auto"
-                                     style="height: 150px; width: 150px;display: block;"
-                                     alt="{{ $pro->image_path }}"
-                                >
-                                <div class="card-body">
-                                    <a href=""><h6 class="card-title">{{ $pro->name }}</h6></a>
-                                    <p>${{ $pro->price }}</p>
-                                    <form action="{{ route('cart.store') }}" method="POST">
-                                        {{ csrf_field() }}
-                                        <input type="hidden" value="{{ $pro->id }}" id="id" name="id">
-                                        <input type="hidden" value="{{ $pro->name }}" id="name" name="name">
-                                        <input type="hidden" value="{{ $pro->price }}" id="price" name="price">
-                                        <input type="hidden" value="{{ $pro->image_path }}" id="img" name="img">
-                                        <input type="hidden" value="{{ $pro->slug }}" id="slug" name="slug">
-                                        <input type="hidden" value="1" id="quantity" name="quantity">
-                                        <div class="card-footer" style="background-color: white;">
-                                            <div class="row">
-                                                <button class="btn btn-secondary btn-sm" class="tooltip-test" title="add to cart">
-                                                    <i class="fa fa-shopping-cart"></i> {{__("add to cart")}}
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 @section('contentLeft')
-    <div class="container" style="margin-top: 80px">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                @if(session()->get('locale')=='ru')
-                    {{(App::setLocale('ru'))}}
-                @endif
-                    <li class="breadcrumb-item active" aria-current="page">{{__('Home ')}}</li>
-                    <li class="breadcrumb-item active"> <a href="{{route("category.list")}}">{{__('Categories')}}</a></li>
-                    <li class="breadcrumb-item active"> <a href="{{route("brand.list")}}">{{__('Brands')}}</a></li>
-            </ol>
-        </nav>
-        <div class="row justify-content-center">
-            <div class="col-lg-12">
-                <div class="row">
-                    <div class="col-lg-7">
-                        <h4>{{__("Products In Our Store")}}</h4>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    @foreach($products as $pro)
-                        <div class="col-lg-3">
-                            <div class="card" style="margin-bottom: 20px; height: auto;">
-                                <img src="/images/{{ $pro->image_path }}"
-                                     class="card-img-top mx-auto"
-                                     style="height: 150px; width: 150px;display: block;"
-                                     alt="{{ $pro->image_path }}"
-                                >
-                                <div class="card-body">
-                                    <a href=""><h6 class="card-title">{{ $pro->name }}</h6></a>
-                                    <p>${{ $pro->price }}</p>
-                                    <form action="{{ route('cart.store') }}" method="POST">
-                                        {{ csrf_field() }}
-                                        <input type="hidden" value="{{ $pro->id }}" id="id" name="id">
-                                        <input type="hidden" value="{{ $pro->name }}" id="name" name="name">
-                                        <input type="hidden" value="{{ $pro->price }}" id="price" name="price">
-                                        <input type="hidden" value="{{ $pro->image_path }}" id="img" name="img">
-                                        <input type="hidden" value="{{ $pro->slug }}" id="slug" name="slug">
-                                        <input type="hidden" value="1" id="quantity" name="quantity">
-                                        <div class="card-footer" style="background-color: white;">
-                                            <div class="row">
-                                                <button class="btn btn-secondary btn-sm" class="tooltip-test" title="add to cart">
-                                                    <i class="fa fa-shopping-cart"></i> {{__("add to cart")}}
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
